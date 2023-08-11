a8b15119c7b17d883262cfd59862829cfa0eaa53
Matheus Alexandre <m.alexandre016@gmail.com>
2023-08-11 18:03:23 -0300

    rota finalizada

--- lib/actions/users.actions.ts
+++ lib/actions/users.actions.ts
@@ -1,16 +1,29 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { User } from "../models/user.models";
 import { connectedToDb } from "../mongoose";
 
 export async function updateUser(
   userId: string,
-  username: string
+  username: string,
+  name: string,
+  bio: string,
+  image: string,
+  path: string
 ): Promise<void> {
   connectedToDb();
 
   await User.findOneAndUpdate(
     { id: userId },
-    { username: username.toLowerCase() }
+    { username: username.toLowerCase(), name, bio, image, onboarded: true },
+    { upsert: true }
   );
+  try {
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (e) {
+    throw new Error(`failed to create/updade ,${e}`);
+  }
 }
